64261a07e051d6d45ca86714073e8c21c718f815
petdrischev <33340136+petdrischev@users.noreply.github.com>
2017-11-05 21:43:47 +0400

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,13 +13,17 @@ def load_data(filepath):
 
 def get_most_frequent_words(text):
     words = [word.strip(',.:;"-()').lower() for word in text.split()]
-    most_freq_words = collections.Counter(words)
+    counted_words = collections.Counter(words)
     top_ten = 10
-    for word, count in most_freq_words.most_common(top_ten):
-        print("Word '{0}' occures: {1} times.".format(word, count))
+    most_freq_words = counted_words.most_common(top_ten)
+    return most_freq_words
 
 
 if __name__ == '__main__':
     filepath = argv[1]
     loaded_text = load_data(filepath)
-    get_most_frequent_words(loaded_text)
+    most_freq_words = get_most_frequent_words(loaded_text)
+    print("\nTop 10 most frequent word in order ascrement in loaded text:")
+    for word in most_freq_words:
+        print('{}'.format(word[0]), end=' ')
+    print('\n')
